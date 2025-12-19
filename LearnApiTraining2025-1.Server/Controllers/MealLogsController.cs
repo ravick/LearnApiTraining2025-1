e42937913f7c11d9fc1f8931b0c524554511af83
Guruzaran <guruzaran@gmail.com>
2025-12-18 22:48:14 -0500

    GET POST APIs with validation - COntroller based APIs

---
+++ LearnApiTraining2025-1.Server/Controllers/MealLogsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LearnApiTraining2025_1.Server.Models;
+
+namespace LearnApiTraining2025_1.Server.Controllers;
+
+[ApiController]
+[Route("api/meals")]
+public class MealLogsController : ControllerBase
+{
+
+    // Temporary in-memory storage (we will replace with DB)
+    private static readonly List<MealLog> _meals = new();
+    [HttpGet]
+    public IActionResult GetAllMeals()
+    {
+        //var meals = new List<MealLog>
+        //{
+        //    new MealLog
+        //    {
+        //        Id = 1,
+        //        MealTime = DateTime.Now.AddHours(-4),
+        //        MealType = "Breakfast",
+        //        Description = "Oats with banana and almonds",
+        //        Calories = 380
+        //    },
+        //    new MealLog
+        //    {
+        //        Id = 2,
+        //        MealTime = DateTime.Now.AddHours(-1),
+        //        MealType = "Lunch",
+        //        Description = "Rice, dal, vegetables",
+        //        Calories = 650
+        //    }
+        //};
+
+        return Ok(_meals);
+    }
+
+    // POST /api/meals
+    [HttpPost]
+    public IActionResult CreateMeal(MealLog meal)
+    {
+        // Simulate auto-increment ID
+        meal.Id = _meals.Count + 1;
+
+        _meals.Add(meal);
+
+        // 201 Created is the correct REST response
+        return CreatedAtAction(
+            nameof(GetAllMeals),
+            new { id = meal.Id },
+            meal
+        );
+    }
+}
---
+++ LearnApiTraining2025-1.Server/Models/MealLog.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnApiTraining2025_1.Server.Models;
+
+public class MealLog
+{
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime MealTime { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string MealType { get; set; } = string.Empty;
+    // Breakfast | Lunch | Dinner | Snack
+
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; set; } = string.Empty;
+
+    [Range(0, 5000)]
+    public int Calories { get; set; }
+}
